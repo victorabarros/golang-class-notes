f7def969d3b05c7c45fcece59bc43fef7a907a42
Victor Barros <victorbarros1130@gmail.com>
2020-03-30 23:02:00 -0300

    learning goroutines

---
+++ codecamp_youtube/main.go
@@ -0,0 +1,35 @@
+// Goroutines
+// https://youtu.be/YS4e4q9oBaU?t=20922
+package main
+
+import (
+	"fmt"
+	// "runtime"
+	"sync"
+	"time"
+)
+
+var wg = sync.WaitGroup{}
+var counter = 0
+var now = time.Now().UTC()
+
+func main() {
+	for i:=0;i<10;i++{
+		wg.Add(2)
+		go sayHello(i)
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello(i int){
+	var dt = time.Now().UTC().Sub(now)
+	fmt.Println(i, "\t", dt, "\t", counter)
+	wg.Done()
+}
+
+func increment(){
+	counter ++
+	wg.Done()
+}
+// Continue with https://youtu.be/YS4e4q9oBaU?t=20906
