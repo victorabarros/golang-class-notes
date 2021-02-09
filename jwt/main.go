200c2e98b3b10b4a0ebed2cc3422415a12c95c36
VictorBarros <victorbarros1130@gmail.com>
2021-02-09 20:34:51 +0000

    epoch to second

--- jwt/main.go
+++ jwt/main.go
@@ -9,15 +9,19 @@ import (
 )
 
 func main() {
+	tokenString, err := buildToken()
+	fmt.Printf("%s\n%s\n", tokenString, err)
+}
+
+func buildToken() (string, error) {
 	// Create the token
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
-		"iat": time.Now().UTC().UnixNano(),
-		"exp": time.Now().UTC().Add(time.Second * 60).UnixNano(),
+		"iat": time.Now().UTC().Unix(),
+		"exp": time.Now().UTC().Add(time.Second * 60).Unix(),
 	})
 
 	privateKey := helper.LoadRSAPrivateKeyFromDisk("./jwtRSA256-private.pem")
 
 	// Sign and get the complete encoded token as a string
-	tokenString, err := token.SignedString(privateKey)
-	fmt.Printf("%s\n%s\n", tokenString, err)
+	return token.SignedString(privateKey)
 }
